3c0a693c386ded482769929d4b835d17e2f379b1
deepter <45136782@qq.com>
2017-03-10 15:37:12 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/DataBase.py
@@ -0,0 +1,99 @@
+# '''
+# Created on 2017年3月8日
+#
+# @author: Administrator
+# '''
+import re
+import jieba.posseg as pseg
+
+
+# 匹配出所有名字,带重复
+def get_all_people_list(file_name):
+    with open(file_name, encoding='utf-8') as f:
+        data = f.read()
+        # 例如20:50:52，要匹配其中的20
+        pa = re.compile(r"\S+\([0-9]+\)")
+        all_people_list = re.findall(pa, data)
+        return all_people_list
+
+
+# 获取所有时间
+def get_time_list():
+    time_list = []
+    for i in range(0, 24):
+        # 这里的判断用于将类似的‘8’ 转化为 ‘08’ 便于和导出数据匹配
+        if i < 10:
+            i = '0' + str(i)
+        else:
+            i = str(i)
+        time_list.append(i)
+    return time_list
+
+
+# 成员列表,不重复
+def get_people_list(all_people_list):
+    people_list_temp = set(all_people_list)
+    people_list = {}
+    for plt in people_list_temp:
+        people_list[plt] = "0"
+    return people_list
+
+
+# 获取不同时间发言次数的set
+def get_time_set(source_file):
+    time_list = get_time_list()
+    with open(source_file, encoding='utf-8') as f:
+        data = f.read()
+        # 例如20:50:52，要匹配其中的20
+        pa = re.compile(r"(\d\d):\d\d:\d\d")
+        times = re.findall(pa, data)
+        time_set = {}
+        for i in time_list:
+            num = 0
+            for time in times:
+                if time == i:
+                    num += 1
+            time_set[i + '点'] = num
+        return time_set
+
+
+# 获取不同人发言次数Set
+def get_people_say_set(source_file):
+    all_people_list = get_all_people_list(source_file)
+    people_list = get_people_list(all_people_list)
+    for people in people_list:
+        count = 0
+        for people_say in all_people_list:
+            if (people_say == people):
+                count += 1
+        people_list[people] = count
+    return people_list
+
+
+def get_hot_noun_counts(source_file):
+    with open(source_file, encoding='utf-8') as f:
+        data = f.read()
+        re_pat = r'[\d-]{10}\s[\d:]{7,8}\s+[^\n]+\d{5,11}\)'  # 记录头数组['2016-06-24 15:42:52  张某(40**21)',…]
+        # li=re.findall(re_pat,data)
+        li_content = re.split(re_pat, data)
+        s = ""
+        for l in li_content:
+            s = s + l
+        seg_list = pseg.cut(s.strip())
+        lists = []
+        for w in seg_list:
+            if (w.flag == "ns"):
+                lists.append(w.word)
+        # print("******群热词统计******")
+        # print("带重复名词总量",len(lists))
+        seg_list_norepeat = set(lists)
+        # print("不重复名词总量",len(seg_list_noRepeat))
+        word_set = {}
+        for seg in seg_list_norepeat:
+            count = 0
+            for ss in lists:
+                if (ss == seg):
+                    count += 1
+            word_set[seg] = count
+        word_tuple_sort = sorted(word_set.items(), key=lambda e: e[1], reverse=True)
+        return word_tuple_sort
---
+++ src/Tools.py
@@ -0,0 +1,41 @@
+# '''
+# Created on 2017年3月8日
+#
+# @author: Administrator
+# '''
+
+
+# 添加sheet,两列结构 最后一个参数是元组
+def add_sheet_type(workbook, sheetname, thistuple, a_title, b_title):
+    print("正在写入数据至 :", sheetname, " 工作簿")
+    # 创建sheet
+    worksheet = workbook.add_worksheet(sheetname)
+    worksheet.set_column('A:A', 40)
+    worksheet.set_column('B:B', 10)
+    # 写入数据，write参数为：行，列，数据
+    i = 1
+    worksheet.write(0, 0, a_title)
+    worksheet.write(0, 1, b_title)
+    for pls in thistuple:
+        worksheet.write(int(i), 0, pls[0])
+        worksheet.write(int(i), 1, pls[1])
+        i += 1
+    print("写入完成")
+
+
+# 添加sheet,两列结构 最后一个参数是Set
+def add_sheet_type2(workbook, sheetname, thistuple, a_title, b_title):
+    print("正在写入数据至 :", sheetname, " 工作簿")
+    # 创建sheet
+    worksheet = workbook.add_worksheet(sheetname)
+    worksheet.set_column('A:A', 40)
+    worksheet.set_column('B:B', 10)
+    # 写入数据，write参数为：行，列，数据
+    i = 1
+    worksheet.write(0, 0, a_title)
+    worksheet.write(0, 1, b_title)
+    for pls in thistuple:
+        worksheet.write(int(i), 0, pls)
+        worksheet.write(int(i), 1, thistuple[pls])
+        i += 1
+    print("写入完成")
